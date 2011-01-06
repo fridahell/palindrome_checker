80891e81efad742ae092c944952ea9c7b6b7b099
Frida Hellden <frida@austinprkonsult.se>
2011-01-06 18:15:44 +0100

    If called without arguments, read string from standard input

--- bin/palindrome_checker.py
+++ bin/palindrome_checker.py
@@ -5,17 +5,19 @@ import re
 
 def main():
 	try:
-		# Make sure the program is called with exactly one argument.
+		# If called without arguments, get string on standard input.
+		if len(sys.argv) == 1:
+			# Set string variable to first command line argument
+			string = str(raw_input("Enter string to check: "))
+		# Otherwise, make sure the program is called with exactly one argument.
 		# sys.argv is the number of args + 1 because
 		# the program's name is in sys.argv[0]
-		if len(sys.argv) != 2:
+		elif len(sys.argv) != 2:
 			print "invalid arguments (%d), usage: %s [string]" % (len(sys.argv), sys.argv[0])
 			return 1
     	
-		# Set string variable to first command line argument
-		string = str(sys.argv[1])
     	
-		# Compile regex matching digits, punctuation characters and blanks
+		# Compile regex matching digits, punctuation characters and blanks.
 		special_characters = re.compile( '[,;.:_!?0-9 -]+')
     	
 		# Remove special characters from string and set our string varialble
@@ -23,23 +25,24 @@ def main():
 		string = special_characters.sub( '', string)
     	
 		# Because the lower() method does not support unicode,
-		# remove any non supported characters before comparing
+		# remove any non supported characters before comparing.
 		invalid_characters = re.compile( '[^a-zA-Z]')
     	
-		# Replace invalid characters with a dot character
+		# Replace invalid characters with a dot character.
 		string = invalid_characters.sub( '.', string)
     	
-		# Convert all upper case characters to lower case
+		# Convert all upper case characters to lower case.
 		string = string.lower()
     	
-		# Reverse string
+		# Reverse string.
 		reversed_string = string[::-1]
     	
-		# Check if string is equal to the reversed string
+		# Check if string is equal to the reversed string.
 		if reversed_string == string:
-			# The string is a palidrome
+			# The string is a palidrome.
 			print "yes"
 		else:
+			# The string is not a palidrome.
 			print "no"
 		return 0
 	except:
