6f3190a27fe0d1368e3a821612157aa1d67574d4
Frida Hellden <frida@austinprkonsult.se>
2011-01-06 18:09:12 +0100

    Fixed typo, palidrome -> palindrome

--- bin/install.sh
+++ bin/install.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-sudo cp palidrome_checker.py /usr/local/bin/palidrome_checker
-sudo chmod 755 /usr/local/bin/palidrome_checker
-echo "palidrome_checker has now been installed"
+sudo cp palindrome_checker.py /usr/local/bin/palindrome_checker
+sudo chmod 755 /usr/local/bin/palindrome_checker
+echo "palindrome_checker has now been installed"
