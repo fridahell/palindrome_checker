3b62600272402aed6cbdafb98713f0dab3120652
Frida Hellden <frida@austinprkonsult.se>
2011-01-06 17:50:53 +0100

    first commit

---
+++ bin/palindrome_checker.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+import sys
+import re
+
+def main():
+	try:
+		# Make sure the program is called with exactly one argument.
+		# sys.argv is the number of arguments + 1 because
+		# the program's name is in sys.argv[0]
+		if len(sys.argv) != 2:
+			print "invalid arguments (%d), usage: %s [string]" % (len(sys.argv), sys.argv[0])
+			return 1
+    	
+		# Set string variable to first command line argument
+		string = str(sys.argv[1])
+    	
+		# Compile regex matching digits, punctuation characters and blanks
+		special_characters = re.compile( '[,;.:_!?0-9 -]+')
+    	
+		# Remove special characters from string and set our string varialble
+		# to the resulting string returned.
+		string = special_characters.sub( '', string)
+    	
+		# Because the lower() method does not support unicode,
+		# remove any non supported characters before comparing
+		invalid_characters = re.compile( '[^a-zA-Z]')
+    	
+		# Replace invalid characters with a dot character
+		string = invalid_characters.sub( '.', string)
+    	
+		# Convert all upper case characters to lower case
+		string = string.lower()
+    	
+		# Reverse string
+		reversed_string = string[::-1]
+    	
+		# Check if string is equal to the reversed string
+		if reversed_string == string:
+			# The string is a palidrome
+			print "yes"
+		else:
+			print "no"
+		return 0
+	except:
+		print "unknown error, usage: %s [string]" % sys.argv[0]
+		return 2
+
+sys.exit(main())
